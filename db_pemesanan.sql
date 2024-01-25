7f6d2f49fc0f123435f13c30a134aebe250bc252
PutraNugraha-new <putranugraha803@gmail.com>
2024-01-25 21:14:05 +0700

    replace db_pemesanan

--- db_pemesanan.sql
+++ db_pemesanan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 24, 2024 at 02:51 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Host: localhost:3306
+-- Generation Time: Jan 25, 2024 at 02:12 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,15 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int NOT NULL,
+  `site_title` varchar(50) NOT NULL,
+  `timezone` varchar(100) NOT NULL,
+  `recaptcha` varchar(5) NOT NULL,
+  `theme` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `site_title`, `timezone`, `recaptcha`, `theme`) VALUES
+(1, 'Dnato System Login', 'Asia/Makassar', 'no', 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_data`
 --
 
 CREATE TABLE `tb_data` (
-  `id_data` int(11) NOT NULL,
+  `id_data` int NOT NULL,
   `tgl_pemesanan` date DEFAULT NULL,
   `jenis_brg` varchar(10) DEFAULT NULL,
-  `banyak_brg` int(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `banyak_brg` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -41,13 +62,13 @@ CREATE TABLE `tb_data` (
 --
 
 CREATE TABLE `tb_detailpemesanan` (
-  `id_detail` int(11) NOT NULL,
-  `id_pemesanan` int(11) NOT NULL,
-  `id_produk` int(11) NOT NULL,
-  `kuantitas` int(11) DEFAULT NULL,
+  `id_detail` int NOT NULL,
+  `id_pemesanan` int NOT NULL,
+  `id_produk` int NOT NULL,
+  `kuantitas` int DEFAULT NULL,
   `harga_satuan` decimal(10,2) DEFAULT NULL,
   `subtotal` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -56,11 +77,11 @@ CREATE TABLE `tb_detailpemesanan` (
 --
 
 CREATE TABLE `tb_keranjang` (
-  `id_keranjang` int(11) NOT NULL,
-  `id_pelanggan` int(11) NOT NULL,
-  `id_produk` int(11) NOT NULL,
-  `kuntitas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_keranjang` int NOT NULL,
+  `id_pelanggan` int NOT NULL,
+  `id_produk` int NOT NULL,
+  `kuntitas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -69,12 +90,12 @@ CREATE TABLE `tb_keranjang` (
 --
 
 CREATE TABLE `tb_pelanggan` (
-  `id_pelanggan` int(11) NOT NULL,
+  `id_pelanggan` int NOT NULL,
   `nama_pelanggan` varchar(100) NOT NULL,
-  `alamat` text DEFAULT NULL,
+  `alamat` text,
   `no_hp` varchar(15) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -83,12 +104,12 @@ CREATE TABLE `tb_pelanggan` (
 --
 
 CREATE TABLE `tb_pemesanan` (
-  `id_pemesanan` int(11) NOT NULL,
-  `id_pelanggan` int(11) NOT NULL,
+  `id_pemesanan` int NOT NULL,
+  `id_pelanggan` int NOT NULL,
   `tgl_pemesanan` date DEFAULT NULL,
   `total_bayar` decimal(10,2) NOT NULL,
   `status_pemesanan` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -97,25 +118,72 @@ CREATE TABLE `tb_pemesanan` (
 --
 
 CREATE TABLE `tb_produk` (
-  `id_produk` int(11) NOT NULL,
+  `id_produk` int NOT NULL,
   `nama_brg` varchar(100) NOT NULL,
   `jenis_brg` varchar(100) DEFAULT NULL,
-  `deskripsi` text DEFAULT NULL,
+  `deskripsi` text,
   `harga` decimal(10,2) DEFAULT NULL,
-  `stok` int(11) DEFAULT NULL,
+  `stok` int DEFAULT NULL,
   `foto_brg` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tb_produk`
 --
 
 INSERT INTO `tb_produk` (`id_produk`, `nama_brg`, `jenis_brg`, `deskripsi`, `harga`, `stok`, `foto_brg`) VALUES
-(4, 'Sofa', 'Kursi Sofa', 'Kursi Sofa Ukura 2x2m, warna Putih', '550000.00', 1, 'sofa.png');
+(4, 'Sofa', 'Kursi Sofa', 'Kursi Sofa Ukura 2x2m, warna Putih', 550000.00, 1, 'sofa.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  `created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `alamat` text,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `role` varchar(10) NOT NULL,
+  `password` text NOT NULL,
+  `last_login` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `banned_users` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `alamat`, `no_hp`, `role`, `password`, `last_login`, `status`, `banned_users`) VALUES
+(1, 'admin@gmail.com', 'Admin', 'Admin', NULL, NULL, '1', 'sha256:1000:UJxHaaFpM44Bj1ka7U58GiSHUx3zRWid:Hq86/PHYj0utJLz2ciHzSehsidHAZX+A', '2017-11-22 09:19:26 PM', 'approved', 'unban'),
+(3, 'putra', 'putra', NULL, 'pky', '081351678870', '2', 'sha256:1000:yhkLETgVloUWIpKaHxnCrMHBpVpihy5k:/iuqzVDoZELQ0h1+/DQ8rjUk6ry2mkVn', '2024-01-24 03:47:54 PM', 'approved', 'unban');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_data`
@@ -142,32 +210,62 @@ ALTER TABLE `tb_produk`
   ADD PRIMARY KEY (`id_produk`);
 
 --
+-- Indexes for table `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_data`
 --
 ALTER TABLE `tb_data`
-  MODIFY `id_data` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_data` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_keranjang`
 --
 ALTER TABLE `tb_keranjang`
-  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_keranjang` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_produk`
 --
 ALTER TABLE `tb_produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_produk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
